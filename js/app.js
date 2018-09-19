654300fc42ab93dc4c0c3cc1e489c4d304cfce51
pedronuness <pedronuness10@gmail.com>
2018-09-19 15:27:01 -0300

    Arquivos Iniciais

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$('.menu-nav a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top,
+			menuHeight = $('header').height();
+	
+	$('html, body').animate({
+		scrollTop: targetOffset -menuHeight -10
+	}, 500)
+});
+
+$('.logo').click(function(e){
+	e.preventDefault();
+	
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+})
+
+$('.bloco').each(function(){
+	var height = $(this).height(),
+			offSetTop = $(this).offset().top,
+			menuHeight = $('header').innerHeight(),
+			id = $(this).attr('id'),
+			$itemMenu = $('a[href="#' + id +'"]');
+	
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(offSetTop - menuHeight < scrollTop && offSetTop + height - menuHeight > scrollTop){
+			$itemMenu.addClass('active');
+		}else{
+			$itemMenu.removeClass('active');
+
+		}
+	})
+});
+
+//Menu Mobile
+
+$('.mobile-btn').click(function(){
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
+});
+
